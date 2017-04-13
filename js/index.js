b3e57c29edcd0aae2790cd167bea57ad9e8a2722
Kabosu <249842959@qq.com>
2017-04-13 20:23:28 +0800

    2333

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$('#dowebok').fullpage({
+//	sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90' , 'red'],
+//	anchors: ['page1', 'page2', 'page3', 'page4' , 'page5'],
+//	menu: '#menu',
+//	continuousVertical: true
+	
+//	'verticalCentered': false,//内容是否垂直居中
+//	'animateAnchor':true,
+    'css3': true,
+    'sectionsColor': ['#254875', '#00FF00', '#254587', '#695684'],//定义背景颜色
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],//定义锚链接
+//  'navigation': true,/*项目导航演示：continuousVertical设置为true  显示右侧点点 false 不显示*/
+	'menu': '#menu',/*绑定菜单：添加菜单项*/
+//	'fixedElements': '#foot',/*绑定菜单：添加菜单项*/
+	'continuousVertical': true
+//	 afterLoad: function(anchorLink, index){
+//	 	//滚动到某一屏后的回调函数.接收 anchorLink 和 index 两个参数，anchorLink 是锚链接的名称，index 是序号，从1开始计算
+//		if(index == 1){
+////			alert(1)
+//			$('.content_first').delay(0).animate({rotate: '360'}, 3000, 'easeInOutElastic');
+//		}
+//	},
+//	onLeave: function(index, direction){
+//		//滚动前的回调函数，接收 index、nextIndex 和 direction 3个参数：index 是离开的“页面”的序号，从1开始计算；
+//		if(index == '1'){
+//			$('.content_first').delay(0).animate({rotate: '360'}, 3000, 'easeInOutElastic');
+//		}
+//	}
+});
